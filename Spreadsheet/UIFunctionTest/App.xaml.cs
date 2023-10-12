b8d0efe7a1157ae6417151d0a6e4df14f6332811
Zelin Yi <1870236214@qq.com>
2023-10-12 09:51:44 -0600

    minor change, for teammate test pull

---
+++ Spreadsheet/UIFunctionTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UIFunctionTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Spreadsheet/UIFunctionTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UIFunctionTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
